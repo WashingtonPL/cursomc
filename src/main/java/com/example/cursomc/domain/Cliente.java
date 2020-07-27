e0528cac7ab9efb85172f5edfdaa21438ac9fbdf
Washington Lacerda <eng.washlacerda@gmail.com>
2020-07-27 15:33:36 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/example/cursomc/domain/Cliente.java
+++ src/main/java/com/example/cursomc/domain/Cliente.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.example.cursomc.domain.enums.TipoCliente;
@@ -36,6 +37,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -88,6 +92,7 @@ public class Cliente implements Serializable {
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
+	
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
@@ -105,6 +110,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,6 +142,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 }
